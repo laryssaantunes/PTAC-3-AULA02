37a069af70f1396a5dfc0ec13dbe249c58fd8c7b
laryssaantunes <larissa.antunes.icloud@gmail.com>
2024-03-06 11:30:23 -0300

    commit css, localizacao e map, dando errado

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import styles from "./menu.module.css";
 import Image from "next/image";
 
-
 export default function Menu(){
     return(
        <header className={styles.cabecalho}>
@@ -10,9 +9,9 @@ export default function Menu(){
             width={100}
             height={100}
             src={"https://www.ifms.edu.br/marcaifms.png"} />
+            src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.m.wikipedia.org%2Fwiki%2FFicheiro%3AMoodle-logo.svg&psig=AOvVaw1_L_WvZVMZiA5IIwKICGAz&ust=1709812662568000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDi5dPK34QDFQAAAAAdAAAAABAJ"}
 
-
-        <h1>Menu</h1>
+        < h1>Menu</h1>
         <nav>
             <ul>
                 <Link href="/">
---
+++ src/app/registro/localizacao/Localizacao.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Localizacao = ()=> {
+    return(
+        <div>
+            <header>
+                <h1>Localização</h1>
+            </header>
+            <main>
+            <Map/>
+            </main>
+            <footer>
+                <p>Mapa</p>
+            </footer>
+        </div>
+    );
+}
+
+
+export default Localizacao;
